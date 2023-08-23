69f0a754f41a732c06c5faeb327c4dee57becd31
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-23 22:16:08 +0900

    connected accountapp

--- Server/server/urls.py
+++ Server/server/urls.py
@@ -19,5 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('account/', include('accountapp.urls')),
+
     path('api-auth/', include('rest_framework.urls')),
 ]
