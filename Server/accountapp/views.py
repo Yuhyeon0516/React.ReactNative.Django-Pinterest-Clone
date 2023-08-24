1a01ccf91a17c6120e5c9c0024a288f4506a6c61
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-24 13:22:30 +0900

    setting accountapp urls, views

--- Server/accountapp/views.py
+++ Server/accountapp/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+
+# 기존 장고 방식
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def hello_world(request):
+    return HttpResponse('Hello_world!');
+
+# DRF 방식
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view()
+def hello_world_drf(request):
+    return Response({"message": "Hello_world!"})
--- Server/server/settings.py
+++ Server/server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2xn0j$4)sx%&735mztlq!rsf7vka9y*59i90%)_jc98($92!_w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
