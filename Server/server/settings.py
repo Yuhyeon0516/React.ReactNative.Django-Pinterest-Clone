01fc9b367aac544883f600de1db2d8a6687508d4
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-30 15:14:11 +0900

    Deleted accountapp

--- Server/server/settings.py
+++ Server/server/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'accountapp',
+    'rest_framework', 
 ]
 
 MIDDLEWARE = [
--- Server/server/urls.py
+++ Server/server/urls.py
@@ -20,7 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('account/', include('accountapp.urls')),
-
     path('api-auth/', include('rest_framework.urls')),
 ]
