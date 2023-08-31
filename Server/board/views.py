5e31a0f7c109e2f3a3e62134a2bcde4642998d06
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-31 13:47:18 +0900

    Added exception in BoardView

--- Server/board/views.py
+++ Server/board/views.py
@@ -9,20 +9,28 @@ from . import models
 
 class CreateBoardView(APIView):
     def post(self, request):
-        board = models.Board.objects.create(
-            token=request.data["token"],
-            board_name=request.data["board_name"],
-            is_secret=request.data["is_secret"],
-        )
+        try:
+            board = models.Board.objects.create(
+                token=request.data["token"],
+                board_name=request.data["board_name"],
+                is_secret=request.data["is_secret"],
+            )
 
-        board.save()
+            board.save()
 
-        return Response(status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_201_CREATED)
+
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class GetBoardView(APIView):
     def get(self, request):
-        queryset = models.Board.objects.filter(token=request.auth)
-        value = queryset.values("board_name")
+        try:
+            queryset = models.Board.objects.filter(token=request.auth)
+            value = queryset.values("board_name")
 
-        return Response(data=value, status=status.HTTP_200_OK)
+            return Response(data=value, status=status.HTTP_200_OK)
+
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
