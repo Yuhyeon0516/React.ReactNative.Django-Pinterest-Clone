dcc04902e49997c036ac483aefcddb1baee0f14a
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-09 11:13:04 +0900

    Created left screen in Save section

--- Client/Web/src/routes/Home.tsx
+++ Client/Web/src/routes/Home.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import Idea from '../components/Idea';
+import Save from '../components/Save';
 import Search from '../components/Search';
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
             <Header />
             <Search />
             <Idea />
+            <Save />
         </div>
     );
 }
